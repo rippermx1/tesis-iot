cf36175113dadb11a3e0bd396a1146c02f92ab88
Carlos Valdes <carlosv@simplemoviles.com>
2019-07-06 22:51:41 -0400

    new/devices

--- app/Http/Controllers/DispositivosController.php
+++ app/Http/Controllers/DispositivosController.php
@@ -21,7 +21,7 @@ class DispositivosController extends Controller
      */
     public function getAll(){
 		try{
-			return response()->json(['devices' => Dispositivos::all()], 200);
+			return response()->json(Dispositivos::all(), 200);
 		}catch(Exception $e){}
 	}
 
